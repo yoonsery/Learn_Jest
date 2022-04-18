2cc24ef4c3716d2bef7060b6e85860acd9bb8ac3
sery <yunsel2@gmail.com>
2022-04-18 17:48:36 +0900

    learn basic jest, async

---
+++ src/basic/test/async.test.js
@@ -0,0 +1,35 @@
+const fetchProduct = require('../../async.js');
+
+describe('Async', () => {
+  it('async - done', (done) => {
+    fetchProduct().then((item) => {
+      expect(item).toEqual({ item: 'Milk', price: 200 });
+      done(); // 💩
+    });
+  });
+
+  it('async - return', () => {
+    return fetchProduct().then((item) => {
+      // `return` 사용하는 방식이 done보다 빠르게 수행 & 깔끔
+      expect(item).toEqual({ item: 'Milk', price: 200 });
+    });
+  });
+
+  it('async - await', async () => {
+    // 콜백함수 앞에 async를 붙여야한다
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: 'Milk', price: 200 });
+  });
+
+  it('async - resolves', () => {
+    return expect(fetchProduct()).resolves.toEqual({
+      // 비동기이므로 return 해주기~
+      item: 'Milk',
+      price: 200,
+    });
+  });
+
+  it('async - reject', () => {
+    return expect(fetchProduct('error')).rejects.toBe('network error');
+  });
+});
---
+++ src/basic/test/calculator.test.js
@@ -0,0 +1,63 @@
+const Calculator = require('../../calculator.js');
+
+describe('Calculator', () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it('inits with 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it('sets', () => {
+    cal.set(3);
+    expect(cal.value).toBe(3);
+  });
+
+  it('clear', () => {
+    cal.set(6);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it('add', () => {
+    cal.set(2);
+    cal.add(5);
+
+    expect(cal.value).toBe(7);
+  });
+
+  it('add should throw an error if value is greater than 100', () => {
+    expect(() => {
+      cal.add(101);
+    }).toThrow('Value can not be greater than 100');
+  });
+
+  it('subtract', () => {
+    cal.set(7);
+    cal.subtract(5);
+
+    expect(cal.value).toBe(2);
+  });
+
+  it('multiply', () => {
+    cal.set(3);
+    cal.multiply(5);
+
+    expect(cal.value).toBe(15);
+  });
+
+  describe('divides', () => {
+    it('0 / 0 = NaN', () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it('1 / 0 = Infinity', () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+  });
+});

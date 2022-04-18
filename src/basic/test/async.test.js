63c8a1268f5bf25acd1adeea60e1f999ab1cf0d0
sery <yunsel2@gmail.com>
2022-04-18 18:08:08 +0900

    Edit import address

--- src/basic/test/async.test.js
+++ src/basic/test/async.test.js
@@ -1,4 +1,4 @@
-const fetchProduct = require('../../async.js');
+const fetchProduct = require('../async.js');
 
 describe('Async', () => {
   it('async - done', (done) => {
--- src/basic/test/calculator.test.js
+++ src/basic/test/calculator.test.js
@@ -1,4 +1,4 @@
-const Calculator = require('../../calculator.js');
+const Calculator = require('../calculator.js');
 
 describe('Calculator', () => {
   let cal;

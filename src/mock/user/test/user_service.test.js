798faa991e1dbf849009ea70d9f12e9171faf0f4
sery <yunsel2@gmail.com>
2022-04-19 14:33:45 +0900

    test user login test using mock

---
+++ src/mock/user/test/user_service.test.js
@@ -0,0 +1,29 @@
+const UserService = require('../user_service');
+const UserClient = require('../user_client');
+jest.mock('../user_client');
+
+describe('UserService', () => {
+  const login = jest.fn(async () => 'success');
+  UserClient.mockImplementation(() => {
+    return {
+      login,
+    };
+  });
+
+  let userService;
+
+  beforeEach(() => {
+    userService = new UserService(new UserClient());
+  });
+
+  it('call login() on UserClient when tries to login', async () => {
+    await userService.login('abc', 'abc');
+    expect(login.mock.calls.length).toBe(1);
+  });
+
+  it('should not call login() on UserClient again if already logged in', async () => {
+    await userService.login('abc', 'abc');
+    await userService.login('abc', 'abc');
+    expect(login.mock.calls.length).toBe(1);
+  });
+});

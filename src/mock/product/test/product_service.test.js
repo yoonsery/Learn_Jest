f55c4f134b8554afcb8b342c66281b1024efbaf9
sery <yunsel2@gmail.com>
2022-04-19 15:35:49 +0900

    Docs: Add readme

--- src/mock/product/test/product_service.test.js
+++ src/mock/product/test/product_service.test.js
@@ -5,11 +5,18 @@ describe('ProductService - Stub', () => {
   let productService;
 
   beforeEach(() => {
+    // 1) Arrange, Given
     productService = new ProductService(new StubProductClient());
   });
 
   it('should filter out only available true', async () => {
+    // 1) Arrange, Given
+    //  productService = new ProductService(new StubProductClient());
+
+    // 2) Act, When
     const items = await productService.fetchAvailableItems();
+
+    // 3) Assert, Then
     expect(items.length).toBe(1);
     expect(items).toEqual([{ item: '🥐', available: true }]);
   });
